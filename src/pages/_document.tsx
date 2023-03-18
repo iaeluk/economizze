6b134ed278a7874ed104cea671314a77f08f846d
Lucas Mendonça <lucas21rio@gmail.com>
2023-03-18 11:40:49 -0300

    code improvements

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="pt">
+      <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+        </Head>
+        <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,13 +17,16 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex flex-col items-center justify-center gap-16 min-w-full min-h-[90%] bg-black text-white">
+      <main className="flex flex-col items-center justify-center gap-16 min-w-full min-h-screen bg-black text-white">
         <button className="absolute top-2 right-2" onClick={() => setMontante(50000)}>REDEFINIR</button>
         <h1 className="text-5xl font-bold">R${montante >= 0 ? (montante.toFixed(2)).replace(".", ",") : Number(0).toFixed(2).replace(".", ",")}</h1>
 
         <div className="flex flex-col gap-3">
-          <input id="valor" className="bg-black outline-none border-2 border-white rounded-md p-3 font-bold text-lg text-center" type="number" value={valorInput} onChange={(e) => setValorInput(e.target?.value)} />
-          <button className="bg-green-700 rounded-md p-4" onClick={() => setMontante(valorInput ? montante - Number(valorInput) : montante)}>INVESTIR NO FUTURO</button>
+          <input id="valor" className="bg-black outline-none border-2 border-white rounded-md p-3 font-bold text-lg text-center" type="number" value={!valorInput ? '': valorInput} onChange={(e) => setValorInput(e.target?.value)} />
+          <button className="bg-green-700 rounded-md p-4" onClick={() => {
+            setMontante(valorInput ? montante - Number(valorInput) : montante)
+            setValorInput(undefined)
+          }}>INVESTIR NO FUTURO</button>
         </div>
       </main>
     </>
