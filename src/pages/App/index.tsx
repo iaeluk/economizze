c9ed391f6460aef9d311c7a5d352ba0123835e06
Lucas Mendonça <lucas21rio@gmail.com>
2023-03-18 18:17:57 -0300

    code improvements

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                 }}>NOVA META</button>
                 <h1 className="text-5xl font-bold">R$ {montante >= 0 ? montante.toFixed(2).replace(".", ",") : Number(0).toFixed(2).replace(".", ",")}</h1>
                 <div className="flex flex-col gap-5">
-                    <input id="valor" className="bg-black outline-none border-2 border-white rounded-md p-3 font-bold text-lg text-center" type="number" value={valorInput} placeholder="INSIRA UM VALOR" onChange={(e) => setValorInput(e.target?.value)} />
+                    <input id="valor" className="bg-black outline-none border-2 border-white rounded-md p-3 font-bold text-lg text-center" type="number" value={valorInput} placeholder="INSIRA UM VALOR" onChange={(e) => setValorInput(e.target?.value)} autoComplete="off"  />
                     <button className="bg-white rounded-md p-4 font-bold text-lg text-black" onClick={() => {
                         const value = valorInput ? Number(valorInput) : 0;
                         setMontante((prevMontante) => prevMontante - value);
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <main className="flex flex-col items-center justify-center gap-16 min-w-full min-h-screen bg-black text-white">
                 <h1 className="text-5xl font-bold">Economizze</h1>
                 <div className="flex flex-col gap-5">
-                    <input id="valor" className="bg-black outline-none border-2 border-white rounded-md p-3 font-bold text-lg text-center" type="number" value={valorInput} placeholder="INSIRA UM VALOR" onChange={(e) => setValorInput(e.target?.value)} />
+                    <input id="valor" className="bg-black outline-none border-2 border-white rounded-md p-3 font-bold text-lg text-center" type="number" value={valorInput} placeholder="INSIRA UM VALOR" onChange={(e) => setValorInput(e.target?.value)} autoComplete="off" />
                     <button className="bg-white rounded-md p-4 font-bold text-lg text-black" onClick={() => {
                         if (Number(valorInput) > 0) {
                             setMontante(Number(valorInput))
