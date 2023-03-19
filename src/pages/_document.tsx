8bee3e87e6bcc5d733403efd4527759ac6d54420
Lucas Mendonça <lucas21rio@gmail.com>
2023-03-19 09:48:21 -0300

    code improvements

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,6 @@ export default function Document() {
   return (
     <Html lang="pt">
       <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Economizze</title>
         <link rel="manifest" href="/manifest.json" />
         <link
           href="/icons/favicon-16x16.png"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
+import Head from 'next/head';
 import Home from './Home';
 import App from './App';
 import { MetaContext } from '@/contexts/MetaContext';
 import { useContext } from 'react';
 
 export default function Index() {
-  const {hasMontante} = useContext(MetaContext)
+  const { hasMontante } = useContext(MetaContext)
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>Economizze</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+      </Head>
       {hasMontante ? <App /> : <Home />}
     </>
   )
