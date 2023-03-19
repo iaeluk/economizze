ef4dc8bfbfff62bdeb88f99b750e0a191b3949a0
Lucas Mendonça <lucas21rio@gmail.com>
2023-03-19 11:22:20 -0300

    code improvements

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MetaContextProvider } from '../contexts/MetaContext'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+   <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width; initial-scale=1; viewport-fit=cover; user-scalable=no"></meta>
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="fullscreen" content="yes" />
+        <meta name="screen-orientation" content="portrait" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <MetaContextProvider>
         <Component {...pageProps} />
       </MetaContextProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,6 @@ export default function Document() {
   return (
     <Html lang="pt">
       <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width; initial-scale=1; viewport-fit=cover; user-scalable=no"></meta>
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="fullscreen" content="yes" />
-        <meta name="screen-orientation" content="portrait" />
         <link rel="manifest" href="/manifest.json" />
         <link
           href="/icons/favicon-16x16.png"
@@ -24,9 +17,8 @@ export default function Document() {
           type="image/png"
           sizes="32x32"
         />
-        <meta name="theme-color" content="#000000" />
       </Head>
-      <body className='font-ibm'>
+      <body className='font-ibm min-h-screen'>
         <Main />
         <NextScript />
       </body>
