97ace92b45afbc95889528db883edff18e8eaa38
Lucas Mendonça <lucas21rio@gmail.com>
2023-03-19 11:10:31 -0300

    code improvements

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width; initial-scale=1; viewport-fit=cover; user-scalable=no"></meta>
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="fullscreen" content="yes" />
