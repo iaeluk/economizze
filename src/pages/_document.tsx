35a7fa43a9968709786ea655225a152df5e9f67e
Lucas Mendonça <79822706+iaeluk@users.noreply.github.com>
2023-03-20 11:24:18 -0300

    code improvements

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,17 +6,18 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/icon-16x16.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/icon-32x32.png"
           rel="icon"
           type="image/png"
           sizes="32x32"
         />
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
       </Head>
       <body className='font-ibm min-h-screen'>
         <Main />
